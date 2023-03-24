bcf561f53c11178df43d1f043bd2fd02bacd6656
KacenkaThomova <katka.thomova@gmail.com>
2023-03-24 15:27:40 +0100

    setValue resolved

--- src/Components/ColorSlider/ColorSlider.jsx
+++ src/Components/ColorSlider/ColorSlider.jsx
@@ -1,21 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import './style.css';
 
 export const ColorSlider = ({baseColor, colorName, onValueChange}) => {
+    const [value, setValue] = useState ('0')
     const handleChange = (event) => {
        onValueChange(event.target.value)
+        setValue (event.target.value)
     }
     return (
         <div className="sliders">
 		    <label htmlFor={baseColor}>{colorName}</label>
 			<input
-                onChange={handleChange}
                 type="range"
                 className={`slider slider--${baseColor}`}
                 id={`${baseColor}Slider`}
                 min="0"
                 max="255"
-                
+                value={value}
+                onChange={handleChange}
             />
         </div>
     )
