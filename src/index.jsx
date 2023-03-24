c7c134f591c58f732b6bedd98d55cc41cfee4a1f
KacenkaThomova <katka.thomova@gmail.com>
2023-03-24 14:38:42 +0100

    cleaned the code

--- src/index.jsx
+++ src/index.jsx
@@ -4,21 +4,22 @@ import './style.css';
 import { ColorSlider } from './Components/ColorSlider/ColorSlider';
 
 const App = () => {
-  const [redValue, setRedValue] = useState ()
-  const [greenValue, setGreenValue] = useState ()
-  const [blueValue, setBlueValue] = useState ()
+  const [redValue, setRedValue] = useState (0)
+  const [greenValue, setGreenValue] = useState (0)
+  const [blueValue, setBlueValue] = useState (0)
   const color = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
- 
+
   return (
     <div className="container">
       <main>
-      <div className="color-panel">
-		<h1>Mixér barev</h1>
-    <ColorSlider baseColor='red' colorName='Cervena' onValueChange={setRedValue} />
-    <ColorSlider baseColor='green' colorName='Zelena' onValueChange={setGreenValue}/>
-    <ColorSlider baseColor='blue' colorName='Modra' onValueChange={setBlueValue}/>
-    <div className="color-box" style={{backgroundColor: `${color}` }} id="color-box"></div>
-	</div>
+        <div className="color-panel">
+          <h1>Mixér barev</h1>
+          <ColorSlider baseColor='red' colorName='Cervena' onValueChange={setRedValue} />
+          <ColorSlider baseColor='green' colorName='Zelena' onValueChange={setGreenValue}/>
+          <ColorSlider baseColor='blue' colorName='Modra' onValueChange={setBlueValue}/>
+          <div className="color-box" style={{backgroundColor: `${color}` }} id="color-box">
+          </div>
+	      </div>
       </main>
       <footer>
         <p>Czechitas, Digitální akademie: Web</p>
